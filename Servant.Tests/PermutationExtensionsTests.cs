574a6c7223351c53ce07be885384d97bec731de9
Drew Noakes <git@drewnoakes.com>
2017-03-27 13:01:27 +0100

    Add PermutationExtensions and tests

---
+++ Servant.Tests/PermutationExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Servant.Tests
+{
+    public sealed class PermutationExtensionsTests
+    {
+        [Fact]
+        public void PermuteTest()
+        {
+            Assert.Equal(new[]
+                {
+                    new[] {1, 2, 3},
+                    new[] {1, 3, 2},
+                    new[] {2, 1, 3},
+                    new[] {2, 3, 1},
+                    new[] {3, 1, 2},
+                    new[] {3, 2, 1}
+                },
+                new[] {1, 2, 3}.Permute());
+
+            Assert.Equal(new[] {new[] {1}}, new[] {1}.Permute());
+            Assert.Equal(new[] {new int[0]}, new int[0].Permute());
+        }
+    }
+}
